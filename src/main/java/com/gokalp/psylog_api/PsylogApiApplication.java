bc7f4f97a9dcda7694eb11afec7407a24219b79d
GokalpBegen <gokalp.begen@sans-technology.com>
2026-03-14 00:44:15 +0300

    Initial commit

---
+++ src/main/java/com/gokalp/psylog_api/PsylogApiApplication.java
@@ -0,0 +1,13 @@
+package com.gokalp.psylog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsylogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsylogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokalp/psylog_api/PsylogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokalp.psylog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsylogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
